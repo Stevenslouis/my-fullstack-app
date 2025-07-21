2e4df9c6801784635a3c7c76be008a89e8c52cba
Stevens Louis <stevenslouis@Stevenss-MacBook-Pro.local>
2025-07-20 23:43:41 -0400

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL, // Your backend URL
+        target: 'https://quiz-app-backend-7i6n.onrender.com', // Your backend URL
         changeOrigin: true,
         secure: false,
       }
